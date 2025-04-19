d5da88de02dbbb5ac65e66c2478e74ccfca2f6ca
xmsagas <74163484+xmsagas@users.noreply.github.com>
2025-04-18 20:20:00 -0600

    exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,6 +10,12 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
+
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
     const personObject = {
       name: newName,
     }
